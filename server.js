e54ea9751533d30b1246925db95731f55d6c5cf4
Connorw2222 <Connor2Werth@gmail.com>
2021-10-25 20:46:29 -0700

    one last time

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
 // Sets express to handle data parsing JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
 const db = fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf8');
 const dbArray = JSON.parse(db)
@@ -27,6 +28,7 @@ app.get('/api/notes', function (req, res) {
     console.log(db)
     res.json(db)
 });
+
 //Post route for notes
 
 //create new notes, write into JSON new notes
@@ -44,11 +46,13 @@ app.post('/api/notes', function (req, res) {
 
 //Displaying notes load on front end (work on it from the front end)
     //fetch request and create new elements. 
-
-
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, 'public/notes.html'));
+    })
 
 
 // Listener
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
+
